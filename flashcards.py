b08d3fa9b0cb5ba10e0c2c0fda9f0d4453f36823
ktostam0 <romanskiba0@gmail.com>
2020-09-14 19:05:26 +0200

    Clean up the code

--- flashcards.py
+++ flashcards.py
@@ -2,17 +2,20 @@ import argparse
 import sys
 import random
 
-parser = argparse.ArgumentParser(description="Digital flashcards software.", epilog='File should contain two frazes separated by separator (default is "; ").')
+# Argparse initialization
+parser = argparse.ArgumentParser(description="Digital flashcards software.", epilog='File should contain two phrases separated by separator (default is "; ").')
 parser.add_argument('-v', '--version', action='version', version='Flashcards 0.1')
-parser.add_argument('-s', '--separator', default="; ", help='sets separator beetween showed and hidden word versions')
+parser.add_argument('-s', '--separator', default="; ", help='sets separator beetween showed and hidden word versions, e.g. ". "')
 parser.add_argument('-r', '--reverse', action='store_true', help='reverses displayed and hidden names')
 parser.add_argument('file_name', help='name of file containing flashcards data')
 
+# Argument parsing
 if len(sys.argv) > 1:  # If enough command line arguments were given
     args = parser.parse_args(sys.argv[1:])  # Processing command line arguments
 else:
     parser.parse_args([])  # Else processing empty list (help should be displayed)
 
+# Opening file
 try:
     with open(args.file_name, 'r') as flashcards_file:
         flashcards = []
@@ -20,13 +23,18 @@ try:
             displayed, hidden = 1, 0
         else:
             displayed, hidden = 0, 1
+        
+        # Parsing file into 2D table
         for i in flashcards_file.readlines():
-            if i.rstrip().lstrip() != "":  # If line isn't empty
-                flashcards.append(i.rstrip().lstrip().split(args.separator))
+            i = i.rstrip().lstrip()
+            if i != "":  # If line isn't empty
+                flashcards.append(i.split(args.separator))
                 if len(flashcards[-1]) < 2:
                     print('Error: Invalid line:', flashcards[-1][0])
                     print('Skipping.')
                     flashcards.pop()
+
+        # Main Loop
         while len(flashcards) > 0:
             random.shuffle(flashcards)
             i = 0
