55ee28bc4d0de972e474cc5f0e50ed6ff9135683
ktostam0 <romanskiba0@gmail.com>
2020-09-27 17:25:35 +0200

    Add error pointer and fix text misspells

--- flashcards.py
+++ flashcards.py
@@ -3,9 +3,9 @@ import sys
 import random
 
 # Argparse initialization
-parser = argparse.ArgumentParser(description="Digital flashcards software.", epilog='File should contain two phrases separated by separator (default is "; ").')
+parser = argparse.ArgumentParser(description='Digital flashcards software.', epilog='File should contain two phrases separated by separator (default is '; ').')
 parser.add_argument('-v', '--version', action='version', version='Flashcards 0.1')
-parser.add_argument('-s', '--separator', default="; ", help='sets separator beetween showed and hidden word versions, e.g. ". "')
+parser.add_argument('-s', '--separator', default='; ', help='sets separator beetween showed and hidden word versions, e.g. '. '')
 parser.add_argument('-r', '--reverse', action='store_true', help='reverses displayed and hidden names')
 parser.add_argument('file_name', help='name of file containing flashcards data')
 
@@ -27,18 +27,18 @@ try:
         # Parsing file into 2D table
         for i in flashcards_file.readlines():
             i = i.rstrip().lstrip()
-            if i != "":  # If line isn't empty
+            if i != '':  # If line isn't empty
                 flashcards.append(i.split(args.separator))
                 if len(flashcards[-1]) < 2:
                     print('Error: Invalid line:', flashcards[-1][0])
                     print('Skipping.')
                     flashcards.pop()
 
-        # Main Loop
+        # Main loop
         iteration = 0
         while len(flashcards) > 0:
             iteration += 1
-            print("Round ", iteration, ":", sep="")
+            print('Round ', iteration, ':', sep='')
             random.shuffle(flashcards)
             i = 0
             while i < len(flashcards):
@@ -48,11 +48,19 @@ try:
                     flashcards.pop(i)
                 else:
                     print('Wrong. Correct answer is:', flashcards[i][hidden])
+                    print('                          ', end='')  # Printing indentation for error pointer
+                    for j in range(len(answer) if len(answer) > len(flashcards[i][hidden]) else len(flashcards[i][hidden])):  # For every character in answer
+                        if j >= len(answer) or j >= len(flashcards[i][hidden]) or answer[j] != flashcards[i][hidden][j]:  # Checking if this is place of error
+                            print('^')  # Printing error pointer
+                            break
+                        else:
+                            print(' ', end='')  # Inreasing indentation
+
                     i += 1
         print('Congratulations!')
 except FileNotFoundError:
     print('Error: File not found')
 except KeyboardInterrupt:
-    print('Interrupted.')
+    print('Interrupted')
 except:
-    print('Error: Invalid file.')
+    print('Error: Invalid file')
